29149d5234e37b2693055b9193de19836d35d052
S.Watanabe <ocelot2828@gmail.com>
2024-10-13 12:03:29 +0900

    feat(apps): update

--- with-next/src/app/layout.tsx
+++ with-next/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import { ServerStylePreview } from "typedcssx/next";
 
 export const metadata: Metadata = {
---
+++ with-remix/app/global.ts
@@ -0,0 +1,10 @@
+import cssx from "typedcssx";
+
+cssx.global({
+  body: {
+    margin: "0 auto",
+  },
+  h1: {
+    fontSize: 32,
+  },
+});
